a90fcbfa82d8f9d645c02dff4b64f73ac7d39349
denysbosiak <b.denis92@gmail.com>
2024-06-13 13:40:35 +0000

    add some folders & files

--- RedditDataEngineering/pipelines/reddit_pipeline.py
+++ RedditDataEngineering/pipelines/reddit_pipeline.py
@@ -1,3 +1,10 @@
-from utils.constants import CLIENT_ID
+from etls.reddit_etl import connect_reddit
+from utils.constants import CLIENT_ID, SECRET
 
 
+def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None):
+    # Connecting to reddit instance
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+    # Exctraction
+    # Transformation
+    # Loading to csv
