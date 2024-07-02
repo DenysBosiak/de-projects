61b7176dda6dcbc641a14f00c2199819004258be
denysbosiak <b.denis92@gmail.com>
2024-07-02 11:25:49 +0000

    change files

--- ApiKafkaStreaming/dags/kafka_stream.py
+++ ApiKafkaStreaming/dags/kafka_stream.py
@@ -42,14 +42,25 @@ def stream_data():
     import json
     import time
     from kafka import KafkaProducer
+    import logging
 
-    res = get_data()
-    res = format_data(res=res)
-    #print(json.dumps(res, indent=3))
 
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
-    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
+    while True:    
+        if time.time() > curr_time + 60: # 1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res=res)
+            #print(json.dumps(res, indent=3))
+
+            
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
 
 with DAG('user_automation',
@@ -63,4 +74,4 @@ with DAG('user_automation',
     )
 
 
-stream_data()
+    streaming_task
