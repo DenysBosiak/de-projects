bafe67f3b0df7b0fa939b868286981040dc4b615
DenysBosiak <b.denis92@gmail.com>
2024-07-08 14:41:39 +0000

    update spark_stream.py

--- ApiKafkaStreaming/spark_stream.py
+++ ApiKafkaStreaming/spark_stream.py
@@ -74,7 +74,7 @@ def create_spark_connection():
         s_conn = SparkSession.builder \
                             .appName('SparkStreaming') \
                             .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
-                                                            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1") \
+                                                            "org.apache.spark:spark-sql-kafka-0-10_2.12-3.4.1") \
                             .config('spark.cassandra.connetion.host', 'localhost') \
                             .getOrCreate()
         
@@ -87,21 +87,23 @@ def create_spark_connection():
 
 
 def connect_to_kafka(spark_conn: SparkSession):
-    # spark_df = spark_conn.createDataFrame([], StructType([]))
+    spark_df = None
 
     try:
         spark_df = spark_conn.readStream \
-            .format('kafka') \
-            .option('kafka.bootstrap.servers', 'broker:9092') \
-            .option('subscribe', 'users_created') \
-            .option('startingOffsets', 'earliest') \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "localhost:9092") \
+            .option("subscribe", "users_created") \
+            .option("startingOffsets", "earliest") \
             .load()
         logging.info("Kafka dataframe created successfully!")
-        
+
     except Exception as e:
         logging.error(f"Kafka dataframe could not be created because: {e}")
 
-    return spark_df
+    return spark_df    
+
+    
 
 
 def create_cassandra_connection():
@@ -118,23 +120,29 @@ def create_cassandra_connection():
     
 
 def create_selection_df_from_kafka(spark_df):
-    schema = StructType([
-        StructField("id", StringType(), False),
-        StructField("first_name", StringType(), False),
-        StructField("last_name", StringType(), False),
-        StructField("gender", StringType(), False),
-        StructField("address", StringType(), False),
-        StructField("postcode", StringType(), False),
-        StructField("email", StringType(), False),
-        StructField("username", StringType(), False),
-        StructField("registered_date", StringType(), False),
-        StructField("phone", StringType(), False),
-        StructField("picture", StringType(), False)
-    ])
+    sel_expanded = None
 
-    sel = spark_df.selectExpr("cast(value as string) as value")
-    sel_expanded = sel.select(from_json(col('value'), schema).alias('data')).select("data.*")
-    print(sel_expanded)
+    try:
+        schema = StructType([
+            StructField("id", StringType(), False),
+            StructField("first_name", StringType(), False),
+            StructField("last_name", StringType(), False),
+            StructField("gender", StringType(), False),
+            StructField("address", StringType(), False),
+            StructField("postcode", StringType(), False),
+            StructField("email", StringType(), False),
+            StructField("username", StringType(), False),
+            StructField("registered_date", StringType(), False),
+            StructField("phone", StringType(), False),
+            StructField("picture", StringType(), False)
+        ])
+
+        sel = spark_df.selectExpr("cast(value as string) as value")
+        sel_expanded = sel.select(from_json(col('value'), schema).alias('data')).select("data.*")
+        print(sel_expanded)
+
+    except Exception as e:
+        print(f"Couldn't create selection dataframe due to {e}")
 
     return sel_expanded
 
@@ -145,9 +153,9 @@ if __name__ == "__main__":
 
     if spark_conn is not None:
         # Connect to kafka with spark connection
-        spark_df = connect_to_kafka(spark_conn)
+        df = connect_to_kafka(spark_conn)
     
-        selection_df = create_selection_df_from_kafka(spark_df)
+        selection_df = create_selection_df_from_kafka(df)
 
         # Create cassandra session
         session = create_cassandra_connection()
@@ -157,9 +165,9 @@ if __name__ == "__main__":
             create_table(session)
             #insert_data(session)
 
-            streaming_query = (selection_df.writeStream.format("org.spsche.spark.sql.cassandra") \
-                                            .option('checkpointLocation', '/tmp/checkpoint') \
-                                            .option('keyspace', 'spark_streams') \
-                                            .option('table', 'created_users') \
-                                            .start())
-            streaming_query.awaitTermination()
+            # streaming_query = (selection_df.writeStream.format("org.apache.spark.sql.cassandra") \
+            #                                 .option('checkpointLocation', '/tmp/checkpoint') \
+            #                                 .option('keyspace', 'spark_streams') \
+            #                                 .option('table', 'created_users') \
+            #                                 .start())
+            # streaming_query.awaitTermination()
