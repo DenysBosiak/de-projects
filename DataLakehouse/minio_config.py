30891ea16877f2576da78236b9d5613fd8f85459
DenysBosiak <120257772+DenysBosiak@users.noreply.github.com>
2024-10-28 16:53:21 +0200

    Update minio_config.py

--- DataLakehouse/minio_config.py
+++ DataLakehouse/minio_config.py
@@ -1,4 +1,4 @@
 config = {
-    'access_key': 'RT0eR04tUPEOrswWdoRv',
-    'secret_key': 'l2GOaSz8a9pXt5CgQonMo84PB56na4QoPbthS3Gl'
+    'access_key': 'your_access_key',
+    'secret_key': 'your_secret_key'
 }
